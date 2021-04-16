b5f1f513cbbb17c334646620cfd62174a4a0f36b
Zeynab-Babashova <82583240+Zeynab-Babashova@users.noreply.github.com>
2021-04-16 11:52:19 +0300

    Add files via upload

---
+++ Task1/task-2nd verion.ps1
@@ -0,0 +1,11 @@
+﻿$XMLfilepath = ‘C:\Users\zeynab.babashova\Desktop\Tasks\Task.xml’
+$XMLDoc = [XML](Get-Content $XMLfilepath) 
+for ($i=0; $i -lt 10; $i++) {
+​​​​​​​​$XMLDoc.SelectNodes("//th") | % {
+$_."#text"=$_."#text".replace("Artist","Artist-$i")
+}
+$XMLDoc.SelectNodes("//th") | % {
+$_."#text"=$_."#text".replace("Title","Title-$i")
+}
+$XMLDoc.Save("C:\Users\zeynab.babashova\Desktop\Tasks\Task-$i.xml")
+}​​​​​​​​​​​
---
+++ Task1/task.ps1
@@ -0,0 +1,8 @@
+﻿$XMLfilepath = ‘C:\Users\zeynab.babashova\Desktop\Tasks\Task.xml’
+$XMLDoc = [XML](Get-Content $XMLfilepath)
+$valueth=$XMLDoc.SelectNodes("//th")
+for ($i=0; $i -lt 10; $i++) {​​​​​​​​
+$valueth[0].Innertext="Title-$i"
+$valueth[1].Innertext="Artist-$i"
+$XMLDoc.Save("C:\Users\zeynab.babashova\Desktop\Tasks\Task-$i.xml")
+}​​​​​​​​​​​​​
